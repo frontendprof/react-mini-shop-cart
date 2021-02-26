bbda9a5946db488fbf904e666d3bb905345ebb40
frontendprof <https://github.com/frontendprof>
2021-02-26 23:40:58 +0500

    Merge branch 'main' of https://github.com/frontendprof/react-mini-shop-cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 
-const Product = ({product}) => {
+const Product = ({product,onAdd}) => {
     return (
         <div>
             <img src={product.image} alt={product.name} className="small"/>
             <h3>{product.name}</h3>
             <div>{product.price}</div>
 
-            <div><button>Add to Cart</button></div>
+            <div>
+                <button onClick={onAdd}>Add to Cart</button>
+                </div>
         </div>
     )
 }
