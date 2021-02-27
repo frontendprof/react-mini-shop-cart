323c6b2b29fb4eeb8ce6ad723b5aecf4bcc01f1e
frontendprof <https://github.com/frontendprof>
2021-02-27 10:02:15 +0500

    adding and removing items  to/from cart

--- src/App.js
+++ src/App.js
@@ -25,6 +25,18 @@ function App() {
     }
   }
 
+  const onRemove=product=>{
+    const exist=cartItems.find(x=>x.id===product.id);
+
+    if(exist.qty===1){
+      setCartItems(cartItems.filter(x=>x.id!==product.id))
+    }else{
+      setCartItems(cartItems.map(x=>
+        x.id===product.id?{...exist,qty:exist.qty-1}:x
+        ))
+    }
+  }
+
   return (
     <div className="App">
 
@@ -33,7 +45,7 @@ function App() {
 
      <div className="row">
        <Main onAdd={onAdd} products={products}></Main>
-       <Basket onAdd={onAdd} cartItems={cartItems}/>
+       <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}/>
      </div>
     </div>
   );
--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 
 
 
-const Basket = ({cartItems,onAdd}) => {
+const Basket = ({cartItems,onAdd,onRemove}) => {
     return (
         <aside className="block col-1">
             <h2>Cart Items</h2>
             <div>
                 {cartItems.length===0&&<div>Cart is Empty</div>}
             </div>
+            {cartItems.map(item=>(
+                <div className="row" key={item.id}>
+                    <div className="col-2">{item.name}</div>
+
+                    <div className="col-2">
+                        <button onClick={()=>onAdd(item)} className="add">+</button>
+                        <button onClick={()=>onRemove(item)} className="remove">-</button>
+        
+                    </div>
+
+                    <div className="col-2 text-right">{item.qty} &times; ${item.price.toFixed(2)}</div>
+                </div>
+
+                
+            ))}
         </aside>
     )
 }
